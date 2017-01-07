b0c8d5b35a97cb8c16b6b21de54b55ade9af0271
Charles Swanson <charlesswanson@mail.com>
2017-01-07 12:17:10 -0800

    Modified demo to render 100 items Shows 6 at a time Slides (slowly) to selected item

--- demo/app.js
+++ demo/app.js
@@ -6,32 +6,49 @@ import ReactDom from 'react-dom';
 
 window.React = React;
 
+const ALL_DATA = Array(100).fill();
+const ITEMS_TO_DISPLAY = 6;
+
 const App = React.createClass({
   mixins: [Carousel.ControllerMixin],
-
-  getInitialState() { return { slideIndex: 0 }; },
-
+  getInitialState() {
+    return {
+      slideIndex: 0, // relative to rendered items
+    };
+  },
+  clickedImg(dataIndex, carouselSelectionDiff) {
+    this.state.carousels.carousel.goToSlide(
+      this.state.slideIndex + carouselSelectionDiff
+    );
+  },
   render() {
+    const {slideIndex} = this.state;
     return (
-      <div style={{width: '50%', margin: 'auto'}}>
+      <div style={{width: '100%', margin: 'auto'}}>
         <Carousel
           ref="carousel"
           data={this.setCarouselData.bind(this, 'carousel')}
           slideIndex={this.state.slideIndex}
-          afterSlide={newSlideIndex => this.setState({ slideIndex: newSlideIndex })}>
-          <img src="http://placehold.it/1000x400&text=slide1"/>
-          <img src="http://placehold.it/1000x400&text=slide2"/>
-          <img src="http://placehold.it/1000x400&text=slide3"/>
-          <img src="http://placehold.it/1000x400&text=slide4"/>
-          <img src="http://placehold.it/1000x400&text=slide5"/>
-          <img src="http://placehold.it/1000x400&text=slide6"/>
+          afterSlide={newSlideIndex => this.setState({ slideIndex: newSlideIndex })}
+          decorators={[]}
+          dragging={false}
+          slidesToShow={ITEMS_TO_DISPLAY}
+          slidesToScroll={ITEMS_TO_DISPLAY}
+          >
+          {
+            ALL_DATA.map((_, dataIndex) => {
+              const carouselSelectionDiff = dataIndex - slideIndex;
+              return (
+                <img
+                  onClick={() => this.clickedImg(dataIndex, carouselSelectionDiff)}
+                  style={{'borderLeft':'1px solid'}}
+                  key={'item' + dataIndex}
+                  src={'http://placehold.it/100x100&text=slide' + dataIndex}
+                  />
+              );
+            })
+          }
         </Carousel>
-        <button onClick={() => this.setState({ slideIndex: 0 })}>1</button>
-        <button onClick={() => this.setState({ slideIndex: 1 })}>2</button>
-        <button onClick={() => this.setState({ slideIndex: 2 })}>3</button>
-        <button onClick={() => this.setState({ slideIndex: 3 })}>4</button>
-        <button onClick={() => this.setState({ slideIndex: 4 })}>5</button>
-        <button onClick={() => this.setState({ slideIndex: 5 })}>6</button>
       </div>
     )
   }
