1eb2263483f573a4c8921c636ad1cbe8dd2b8afb
Charles Swanson <charlesswanson@mail.com>
2017-01-07 12:17:42 -0800

    Only render a max of 6 items at a time. Repros bug: click 1, then 2, then 3 On the final click, 4 is shown instead of 3

--- demo/app.js
+++ demo/app.js
@@ -6,38 +6,62 @@ import ReactDom from 'react-dom';
 
 window.React = React;
 
-const ALL_DATA = Array(100).fill();
-const ITEMS_TO_DISPLAY = 6;
+// [0,1,2,3,...99]
+const ALL_DATA = Array.apply(null, {length: 100}).map(Number.call, Number);
+const ITEMS_TO_DISPLAY = 2;
 
 const App = React.createClass({
   mixins: [Carousel.ControllerMixin],
   getInitialState() {
     return {
-      slideIndex: 0, // relative to rendered items
+      selectedCarouselIndex: 0, // relative to rendered items
+      selectedDataIndex: 0, // where we are relative to start
+      firstDataIndexToRender: 0,
     };
   },
   clickedImg(dataIndex, carouselSelectionDiff) {
     this.state.carousels.carousel.goToSlide(
-      this.state.slideIndex + carouselSelectionDiff
+      this.state.selectedCarouselIndex + carouselSelectionDiff
     );
   },
+  afterSlide(newSelectedCarouselIndex) {
+    console.log('after slide to index: ', newSelectedCarouselIndex);
+    const {origImgIds, onImgSelected} = this.props;
+    const newImgId = ALL_DATA[newSelectedCarouselIndex];
+
+    const {firstDataIndexToRender, selectedDataIndex, selectedCarouselIndex} = this.state;
+    const moveDiff = newSelectedCarouselIndex - selectedCarouselIndex;
+
+    const newSelectedDataIndex = selectedDataIndex + moveDiff;
+    const newFirstDataIndexToRender = Math.max(0, newSelectedDataIndex - ITEMS_TO_DISPLAY);
+    const dataIndexRenderDiff = newFirstDataIndexToRender - firstDataIndexToRender;
+    const newSelectedCarouselIndexAdjusted = selectedCarouselIndex + moveDiff - dataIndexRenderDiff;
+    this.setState({
+      selectedCarouselIndex: newSelectedCarouselIndexAdjusted,
+      selectedDataIndex: newSelectedDataIndex,
+      firstDataIndexToRender: newFirstDataIndexToRender,
+    });
+  },
   render() {
-    const {slideIndex} = this.state;
+    const {firstDataIndexToRender, selectedDataIndex, selectedCarouselIndex} = this.state;
+    const lastDataIndexToRender = Math.min(ALL_DATA.length - 1, selectedDataIndex + ITEMS_TO_DISPLAY * 2 - 1);
+    console.log('render data indices', firstDataIndexToRender, 'to', lastDataIndexToRender);
     return (
       <div style={{width: '100%', margin: 'auto'}}>
         <Carousel
           ref="carousel"
           data={this.setCarouselData.bind(this, 'carousel')}
-          slideIndex={this.state.slideIndex}
-          afterSlide={newSlideIndex => this.setState({ slideIndex: newSlideIndex })}
+          slideIndex={this.state.selectedCarouselIndex}
+          afterSlide={newSelectedCarouselIndex => this.afterSlide(newSelectedCarouselIndex)}
           decorators={[]}
           dragging={false}
+          speed={2000}
           slidesToShow={ITEMS_TO_DISPLAY}
           slidesToScroll={ITEMS_TO_DISPLAY}
           >
           {
-            ALL_DATA.map((_, dataIndex) => {
-              const carouselSelectionDiff = dataIndex - slideIndex;
+            ALL_DATA.slice(firstDataIndexToRender, lastDataIndexToRender + 1).map((dataIndex, renderedItemIndex) => {
+              const carouselSelectionDiff = renderedItemIndex - selectedCarouselIndex;
               return (
                 <img
                   onClick={() => this.clickedImg(dataIndex, carouselSelectionDiff)}
